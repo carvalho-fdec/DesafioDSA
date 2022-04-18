fec9742bd77ddc3923ed616b6511cce87de48968
Fernando <carvalho.fdec@gmail.com>
2022-04-18 19:52:33 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -15,9 +15,23 @@ plt.ylabel('Número de veículos')
 plt.title('Distribuição de veículos por ano de registro')
 plt.show()
 
+# boxplot para verificar a distribuição de preços por tipo de veículo
+plt.boxplot(df['price'] , labels=df['vehicleType'])
+plt.xlabel('Tipo de veículo')
+plt.ylabel('Preço')
+plt.title('Distribuição de preços por tipo de veículo')
+plt.show()
 
-
-
-
-
-
+# countplot para verificar a distribuição de veículos por tipo de veículo
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.title('Distribuição de veículos por tipo de veículo')
+plt.xlabel('Tipo de veículo')
+plt.ylabel('Número de veículos')
+plt.xticks(rotation=90)
+plt.subplot(1,2,2)
+plt.title('Distribuição de veículos por tipo de veículo')
+plt.xlabel('Tipo de veículo')
+plt.ylabel('Número de veículos')
+plt.xticks(rotation=90)
+plt.show()
